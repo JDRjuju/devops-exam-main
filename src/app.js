75c9289b5372edd0a46080bd6002818adff6071f
JDRjuju <94639280+JDRjuju@users.noreply.github.com>
2023-01-20 11:02:58 +0100

    add message of caracters

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ try {
     const gamerTagInput = document.getElementById("gamerTagInput");
     const checkButton = document.getElementById("gamerTagCheckButton");
     const feedbackMessage = document.getElementById("gamerTagFeedback");
+    const checkCaracters = require("./services/validations");
 
     var gamerTagValue;
     var feedbackMessageText;
@@ -14,6 +15,11 @@ try {
 
     checkButton.addEventListener("click", () => {
         // TODO: Add the logic to display the correct feedback message (error and validation)
+        feedbackMessage = checkCaracters(gamerTagValue)
+            ? "Gamer tag must have a special caracters"
+            : "Gamer tag is valid";
+        feedbackMessage.textContent = feedbackMessageText;
+    
         feedbackMessageText = isEmpty(gamerTagValue)
             ? "Gamer tag cannot be empty"
             : "Gamer tag is valid";
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,5 +1,16 @@
 // TODO: Add here the function to validate
 
+module.exports.checkCaracters = (caracters) => {
+    
+  
+    const regex = /[^A-Z]|/g;
+    const found = caracters.match(regex);
+    console.log(found);
+    if (found === null || found.length === 0) return true;
+    else return false;
+};
+
+
 const isEmpty = (label) => !label || label.length === 0;
 
 exports.isEmpty = isEmpty;
